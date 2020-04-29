dbb53bb5d31134e3d10099337721ddbe2d79dc01
R-Kawabe <dragontree7@gmail.com>
2020-04-30 01:42:43 +0900

    update 20200430

--- src/Service/fetch.js
+++ src/Service/fetch.js
@@ -46,9 +46,7 @@ async function fetchTrain(request, param) {
         if (!result.ok) {
             throw new Error("error");
         }
-        console.log(result);
         const resultData = await result.json(result || "null");
-        console.log(resultData);
         return resultData;
     } catch (e) {
         elem.innerText = e.message;
